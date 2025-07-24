14f16f93fac8bab91c3d02528658f37799638486
Eugene Prigornitskiy <ionixxx@gmail.com>
2025-07-24 10:47:13 +0300

    remove unused 'Recurrent' field from initialization map

--- init.go
+++ init.go
@@ -52,7 +52,6 @@ func (i *InitRequest) GetValuesForToken() map[string]string {
 		"NotificationURL": i.NotificationURL,
 		"SuccessURL":      i.SuccessURL,
 		"FailURL":         i.FailURL,
-		"Recurrent":       i.Recurrent,
 	}
 	serializeUintToMapIfNonEmpty(&v, "Amount", i.Amount)
 	return v
